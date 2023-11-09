0c14ab475f9d8e1bc526d2b3504c0cb2c7dd51db
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2023-11-09 20:01:48 +0200

    feat: introduced node build.

---
+++ node/update.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+set -e
+
+
+cd "$(dirname "$(readlink -f "$0")")"
+for major in 20 21 ; do
+  version=$(docker run --rm node:${major}-bookworm-slim --version)
+  version=$(echo ${version} | cut -c2-)
+  minor=$(echo ${version} | cut -d. -f2)
+
+  image=akorn/node
+  echo building ${version} ...
+
+  docker buildx build \
+    --build-arg NODE_VERSION=${version} \
+    --tag ${image} \
+    --tag ${image}:${major} \
+    --tag ${image}:${major}.${minor} \
+    --tag ${image}:${version} \
+    --platform linux/amd64,linux/arm64 \
+    --push \
+    .
+
+done
