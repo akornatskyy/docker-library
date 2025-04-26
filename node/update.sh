c936bd7e49f864cac700d8fab071bd11ae82f299
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2025-04-26 20:28:32 +0300

    chore: add build date as a build argument in update script.

--- node/update.sh
+++ node/update.sh
@@ -2,6 +2,8 @@
 set -e
 
 
+build_date=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
+
 cd "$(dirname "$(readlink -f "$0")")"
 for major in 20 22 23 24 ; do
   version=$(docker run --rm node:${major}-slim --version)
@@ -13,6 +15,7 @@ for major in 20 22 23 24 ; do
 
   docker buildx build \
     --build-arg NODE_VERSION=${version} \
+    --build-arg BUILD_DATE=${build_date} \
     --tag ${image} \
     --tag ${image}:${major} \
     --tag ${image}:${major}.${minor} \
