e955e7f0b519a9690587a9c269b8afbc5227e920
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2025-12-01 20:15:42 +0200

    chore: added node builds for debian 13.

--- node/update.sh
+++ node/update.sh
@@ -2,26 +2,35 @@
 set -e
 
 
+image=akorn/node
 build_date=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 
 cd "$(dirname "$(readlink -f "$0")")"
-for major in 20 22 23 24 25 ; do
-  version=$(docker run --rm node:${major}-slim --version)
-  version=$(echo ${version} | cut -c2-)
-  minor=$(echo ${version} | cut -d. -f2)
+for os in bookworm,12 trixie,13 ; do
+  debian_codename=${os%%,*}
+  debian_version=${os##*,}
 
-  image=akorn/node
-  echo building ${version} ...
+  for major in 22 24 25 ; do
+    version=$(docker run --rm node:${major}-${debian_codename}-slim --version)
+    version=$(echo ${version} | cut -c2-)
+    minor=$(echo ${version} | cut -d. -f2)
 
-  docker buildx build \
-    --build-arg NODE_VERSION=${version} \
-    --build-arg BUILD_DATE=${build_date} \
-    --tag ${image} \
-    --tag ${image}:${major} \
-    --tag ${image}:${major}.${minor} \
-    --tag ${image}:${version} \
-    --platform linux/amd64,linux/arm64 \
-    --push \
-    .
+    docker buildx build \
+      --build-arg NODE_VERSION=${version} \
+      --build-arg BUILD_DATE=${build_date} \
+      --build-arg DEBIAN_CODENAME=${debian_codename} \
+      --build-arg DEBIAN_VERSION=${debian_version} \
+      --tag ${image} \
+      --tag ${image}:${major} \
+      --tag ${image}:${major}.${minor} \
+      --tag ${image}:distroless-debian${debian_version} \
+      --tag ${image}:${major}-distroless-debian${debian_version} \
+      --tag ${image}:${major}.${minor}-distroless-debian${debian_version} \
+      --platform linux/amd64,linux/arm64 \
+      --push \
+      .
 
+    docker run --rm ${image} --version
+
+  done
 done
